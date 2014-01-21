306e7015bb93b8cac598bb7f30985b36a4ece97f
lincoln.liu <xianlinbox@gmail.com>
2014-01-21 16:50:16 +1000

    Initial Commit

---
+++ iOS7_New/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  iOS7_New
+//
+//  Created by lincoln.liu on 1/21/14.
+//  Copyright (c) 2014 xianlinbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iOS7_NewTests/iOS7_NewTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS7_NewTests.m
+//  iOS7_NewTests
+//
+//  Created by lincoln.liu on 1/21/14.
+//  Copyright (c) 2014 xianlinbox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS7_NewTests : XCTestCase
+
+@end
+
+@implementation iOS7_NewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
